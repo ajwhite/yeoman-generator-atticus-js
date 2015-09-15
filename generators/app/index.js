27f88df79a2833d33ee239770c83bb2661725228
Atticus White <contact@atticuswhite.com>
2015-09-15 07:57:41 -0400

    build templates

--- generators/app/index.js
+++ generators/app/index.js
@@ -10,10 +10,36 @@ Generator = module.exports = function Generator () {
   this.argument('appname', {type: String, required: false});
   this.appname = this.appname || path.basename(process.cwd());
   this.appname = _.camelCase(this.appname);
+  this.appnameSlug = _.kebabCase(this.appname);
+  this.gulpNotifyOnErrorString = 'Error: <%= error.message %>';
 };
 
 util.inherits(Generator, yeoman.generators.Base);
 
 Generator.prototype.welcome = function () {
   this.log(yosay('Hello motherfucker!'));
+};
+//
+// Generator.prototype.installDependencies = function () {
+//   this.npmInstall([
+//     'gulp',
+//     'gulp-less',
+//     'gulp-minify-css',
+//     'gulp-uglify',
+//     'gulp-plumber',
+//     'gulp-notify',
+//     'gulp-jscs',
+//     'gulp-jshint'
+//   ], {saveDev: true});
+// };
+
+Generator.prototype.buildTemplates = function () {
+  this.template('_package.json', 'package.json');
+  this.template('_travis.yml', 'travis.yml');
+  this.template('README.md', 'README.md');
+  this.template('gulpfile.js', 'gulpfile.js');
+  this.copy('gitignore', '.gitignore');
+  this.copy('jscsrc', '.jscsrc');
+  this.copy('jshintrc', '.jshintrc');
+  this.copy('editorconfig', '.editorconfig');
 };
